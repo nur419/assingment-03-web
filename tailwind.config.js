4074021d08f94673ce1d6be0fd471e27094afa98
nur419 <nurnobisarker0003n@gmail.com>
2025-02-01 22:05:48 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/c1-assets/cat-big.png')",
+          'footer-texture': "url('/c1-assets/cute-pet-mid.png')",
+        }
+      }
+    }
+  }
